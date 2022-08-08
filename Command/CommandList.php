cbc5095a0986e21a8d331cfd0dfabd18d2eec007
jisse Reitsma <jisse@yireo.com>
2022-08-08 14:35:54 +0200

    Initial files from Yireo_ExtensionChecker

---
+++ Command/CommandList.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types=1);
+
+namespace Yireo\ExtensionCheckerCli\Console;
+
+use Magento\Framework\ObjectManagerInterface;
+use Yireo\ExtensionChecker\Console\Command\ScanCommand;
+use Magento\Framework\Console\CommandListInterface;
+
+/**
+ * Provide list of CLI commands to be available for not-installed application
+ */
+class CommandList implements CommandListInterface
+{
+    /**
+     * @var ObjectManagerInterface
+     */
+    private $objectManager;
+
+    /**
+     * @param ObjectManagerInterface $objectManager
+     */
+    public function __construct(ObjectManagerInterface $objectManager)
+    {
+        $this->objectManager = $objectManager;
+    }
+
+    /**
+     * Gets list of command classes
+     *
+     * @return string[]
+     */
+    private function getCommandsClasses(): array
+    {
+        return [ScanCommand::class];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getCommands(): array
+    {
+        $commands = [];
+        foreach ($this->getCommandsClasses() as $class) {
+            if (class_exists($class)) {
+                try {
+                    $commands[] = $this->objectManager->get($class);
+                } catch (\Throwable $throwable) {}
+            } else {
+                throw new \RuntimeException('Class ' . $class . ' does not exist');
+            }
+        }
+
+        return $commands;
+    }
+}
+
